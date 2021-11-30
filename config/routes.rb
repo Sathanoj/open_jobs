1c21e705c5287149a181dce9733ab9aef20efbc7
Sathanoj <69756956+Sathanoj@users.noreply.github.com>
2021-11-29 21:44:00 -0300

    Criando rotas para applicants positions e companies

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :companies, only: [ :new, :edit, :create, :update]
+  resources :companies, only: [ :new, :edit, :create, :update ]
+  resources :positions, only: [ :index, :new, :edit, :show ]
+  resources :applicants, only: [ :index, :new ]
 end
